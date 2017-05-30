cdb3f6784e4e5023a3ae644205c8d33893f0a912
David Ferris <david.j.ferris.21@gmail.com>
2017-05-30 09:56:13 -0700

    renamed files

--- secret_interview/google1.py
+++ secret_interview/google1.py
@@ -1,92 +1,25 @@
-import operator
+def answer(l):
+	l = [i.split(".") for i in l]
+	l = sorted(l, key=lambda x: x[0]) #sort by major number
+	return sortSubList(l,0)
 
-def answer(s):
-    temp = ""
-    for c in s:
-        if (ord(c) <= 122 and ord(c) >= 97):
-            temp += chr(-1*ord(c) + 219);
-            print(ord(c))
-        else:
-        	temp += c
-    return temp
+def sortSubList(l, i):
+	a = 0
+	b = 1
+	if (len(l[0]) == i+1):
+		return sorted(l, key=lambda x: x[i]) #sort by major number
+	
+	while (b < len(l)):
+		while (len(l[a]) > i+1): #numbers without minor digits don't require sorting
+			a+=1
+			b+=1
+		while (l[a][i] == l[b][i]):
+			b+=1
+		if ((b-a) > 1):
+			l[a:b] = sortSubList(l[a:b], i+1) # sort section of list in place
+		a = b+1
+		b += 2
+	return l
 
-def numMoves(x,y):
-    x = abs(x)
-    y = abs(y)
-    if y > x:
-        temp=y
-        y=x
-        x=temp  
-    if (x==2 and y==2):
-        return 4
-    if (x==1 and y==0):
-        return 3
-
-    if(y == 0 or float(y) / float(x) <= 0.5):
-        xClass = x % 4
-        if (xClass == 0):
-            initX = x/2
-        elif(xClass == 1):
-            initX = 1 + (x/2)
-        elif(xClass == 2):
-            initX = 1 + (x/2)
-        else:
-            initX = 1 + ((x+1)/2)
-
-        if (xClass > 1):
-            return initX - (y%2)
-        else:
-            return initX + (y%2)
-    else:
-        diagonal = x - ((x-y)/2)
-        if((x-y)%2 == 0):
-            if (diagonal % 3 == 0):
-                return (diagonal/3)*2
-            if (diagonal % 3 == 1):
-                return ((diagonal/3)*2)+2
-            else:
-                return ((diagonal/3)*2)+2
-        else:
-            return ((diagonal/3)*2)+1
-
-def compare(a,b):
-    if (len(a) == 0 and len(b) != 0): return -1
-    if (len(b) == 0 and len(a) != 0): return 1
-    if (len(a) == 0 and len(b) == 0): return 0
-    if (int(a[0]) > int(b[0])): return 1
-    elif (int(a[0]) < int(b[0])): return -1
-    return compare(a[1:], b[1:])
-
-def concat(a):
-    string = ""
-    for x in a:
-        string+=str(x)
-        string+="."
-    return string[:-1]
-
-def answer2(l):
-    l = [x.split(".") for x in l]
-    l.sort(compare)
-    l = [concat(x) for x in l]
-    return l
-
-def answer3(m):
-    order = range(0,len(m))
-    m = [[1 if sum(m[i]) is 0 and i==j else m[i][j] for j in range(0,len(m))] for i in range(0,len(m))] # Add missing ones to terminal rows
-    m = [[(float(i)/sum(j)) for i in j] for j in m] # Normalize the Matrix Rows
-    
-    return m
-
-
-M = [
-[0,1,0,0,0,1],
-[4,0,0,3,2,0],
-[0,0,0,0,0,0],
-[0,0,0,0,0,0],
-[0,0,0,0,0,0],
-[0,0,0,0,0,0]
-]
-A = answer3(M)
-for row in A:
-    print(row)
-
+l = ["1.1.2", "1.0", "1.3.3", "1.0.12", "1.0.2", "2.0"]
+print(answer(l))
--- secret_interview/google2.py
+++ secret_interview/google2.py
@@ -1,51 +1,92 @@
-class Node(object):
-    def __init__(self):
-        self.nodes = []
-        self.data = None
+import operator
 
-    def sortNodes(self):
-		self.nodes.sort(key=lambda x: x.data, reverse=False)
+def answer(s):
+    temp = ""
+    for c in s:
+        if (ord(c) <= 122 and ord(c) >= 97):
+            temp += chr(-1*ord(c) + 219);
+            print(ord(c))
+        else:
+        	temp += c
+    return temp
 
-    def addNodes(self, l):
-    	if (len(l) == 0):
-    		return
-    	for n in self.nodes:
-    		if (n.data == l[0]):
-    			n.addNodes(l[1:])
-    			return
-    	n = Node()
-    	n.data = l[0]
-    	self.nodes.append(n)
-    	n.addNodes(l[1:])
+def numMoves(x,y):
+    x = abs(x)
+    y = abs(y)
+    if y > x:
+        temp=y
+        y=x
+        x=temp  
+    if (x==2 and y==2):
+        return 4
+    if (x==1 and y==0):
+        return 3
 
-    def traversal(self):
-    	print(self.data)
-    	for x in self.nodes:
-    		x.traversal()
+    if(y == 0 or float(y) / float(x) <= 0.5):
+        xClass = x % 4
+        if (xClass == 0):
+            initX = x/2
+        elif(xClass == 1):
+            initX = 1 + (x/2)
+        elif(xClass == 2):
+            initX = 1 + (x/2)
+        else:
+            initX = 1 + ((x+1)/2)
 
-    def addtoArray(self, main_arr, path):
-    	path.append(self.data)
-    	if (len(self.nodes) == 0): #leaf node
-    		main_arr.append(path)
-    	else:
-	    	for n in self.nodes:
-	    		n.addtoArray(main_arr, path)
+        if (xClass > 1):
+            return initX - (y%2)
+        else:
+            return initX + (y%2)
+    else:
+        diagonal = x - ((x-y)/2)
+        if((x-y)%2 == 0):
+            if (diagonal % 3 == 0):
+                return (diagonal/3)*2
+            if (diagonal % 3 == 1):
+                return ((diagonal/3)*2)+2
+            else:
+                return ((diagonal/3)*2)+2
+        else:
+            return ((diagonal/3)*2)+1
+
+def compare(a,b):
+    if (len(a) == 0 and len(b) != 0): return -1
+    if (len(b) == 0 and len(a) != 0): return 1
+    if (len(a) == 0 and len(b) == 0): return 0
+    if (int(a[0]) > int(b[0])): return 1
+    elif (int(a[0]) < int(b[0])): return -1
+    return compare(a[1:], b[1:])
+
+def concat(a):
+    string = ""
+    for x in a:
+        string+=str(x)
+        string+="."
+    return string[:-1]
+
+def answer2(l):
+    l = [x.split(".") for x in l]
+    l.sort(compare)
+    l = [concat(x) for x in l]
+    return l
+
+def answer3(m):
+    order = range(0,len(m))
+    m = [[1 if sum(m[i]) is 0 and i==j else m[i][j] for j in range(0,len(m))] for i in range(0,len(m))] # Add missing ones to terminal rows
+    m = [[(float(i)/sum(j)) for i in j] for j in m] # Normalize the Matrix Rows
+    
+    return m
 
 
-def answer(l):
-	l = [i.split(".") for i in l]
-	root = Node()
-	# Generate tree
-	for x in l:
-		root.addNodes(x)
-	root.sortNodes() # recursively sorting nodes enables an in-order traversal
-	root.traversal()
-	arr = []
-	root.addtoArray(arr, []) # in-order traversal with appending
-	return arr
-
-#L = ["1.1.2", "1.0", "1.3.3", "1.0.12", "1.0.2", "2.0", "2"]
-L = ["1.1.2","2"]
-answer(L)
-print(answer(L))
+M = [
+[0,1,0,0,0,1],
+[4,0,0,3,2,0],
+[0,0,0,0,0,0],
+[0,0,0,0,0,0],
+[0,0,0,0,0,0],
+[0,0,0,0,0,0]
+]
+A = answer3(M)
+for row in A:
+    print(row)
 
