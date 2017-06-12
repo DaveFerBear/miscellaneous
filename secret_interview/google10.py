4a5799d8bd13189e75cd03725c695ff4822eb091
David Ferris <david.j.ferris.21@gmail.com>
2017-06-12 09:52:28 -0700

    changes to algo #10

--- secret_interview/google10.py
+++ secret_interview/google10.py
@@ -74,48 +74,49 @@ class Graph:
 
 
 def answer(entrances, exits, path):
-	#Edmonds-Karp Maximum Flow Implementation
-	#https://en.wikipedia.org/wiki/Ford%E2%80%93Fulkerson_algorithm#Python_implementation_of_Edmonds-Karp_algorithm
+    #Edmonds-Karp Maximum Flow Implementation
+    #https://en.wikipedia.org/wiki/Ford%E2%80%93Fulkerson_algorithm#Python_implementation_of_Edmonds-Karp_algorithm
 
-	# #Construct graph
-	# num_rooms = len(path)
-	# G = {a:{b:path[a][b] for b in range(num_rooms) if (path[a][b]!=0 and a!=b)} for a in range(num_rooms)}
-	
-	# #Add dummy source and sink
-	# source_weights = [sum([row[x] for row in path]) for x in entrances]
-	# sink_weights = [sum([row[x] for row in path]) for x in exits]
-	# G['s'] = {entrances[i]:source_weights[i] for i in range(len(entrances))} #source
-	# G['t'] = {exits[i]:sink_weights[i] for i in range(len(entrances))} #sink
+    # #Construct graph
+    # num_rooms = len(path)
+    # G = {a:{b:path[a][b] for b in range(num_rooms) if (path[a][b]!=0 and a!=b)} for a in range(num_rooms)}
+    
+    # #Add dummy source and sink
+    # source_weights = [sum([row[x] for row in path]) for x in entrances]
+    # sink_weights = [sum([row[x] for row in path]) for x in exits]
+    # G['s'] = {entrances[i]:source_weights[i] for i in range(len(entrances))} #source
+    # G['t'] = {exits[i]:sink_weights[i] for i in range(len(entrances))} #sink
+    
+    g = Graph(path)
+    firstTry = g.EdmondsKarp(entrances[-1],exits[-1])
+    n = len(path)
 
-	n = len(path)
-
-	#Add dummy source and sink to matrix
-	source_weights = [sum(path[row]) for row in entrances]
-	sink_weights = [sum([row[x] for row in path]) for x in exits]
-	path = [row+[0,0] for row in path]
-	[path.append([0 for x in range(n+2)]) for y in range(2)]
-	for i,e in enumerate(exits):
-		path[e][n+1] = sink_weights[i]
-	for i,e in enumerate(entrances):
-		path[n][i] = source_weights[i]
-	
-	g = Graph(path)
-	return g.EdmondsKarp(n,n+1)
+    #Add dummy source and sink to matrix
+    source_weights = [sum(path[row]) for row in entrances]
+    sink_weights = [sum([row[x] for row in path]) for x in exits]
+    path = [row+[0,0] for row in path]
+    [path.append([0 for x in range(n+2)]) for y in range(2)]
+    for i,e in enumerate(exits):
+        path[e][n+1] = sink_weights[i]
+    for i,e in enumerate(entrances):
+        path[n][i] = source_weights[i]
+    
+    g = Graph(path)
+    secondTry = g.EdmondsKarp(n,n+1)
+    return min([firstTry,secondTry])
+    
 
 
 entrances = [0]
 exits = [3]
 path = [[0, 7, 0, 0], [0, 0, 6, 0], [0, 0, 0, 8], [9, 0, 0, 0]]
-
-entrances = [0, 1]
-exits = [4, 5]
-path = [[0, 0, 4, 6, 0, 0],
-		[0, 0, 5, 2, 0, 0],
-		[0, 0, 0, 0, 4, 4],
-		[0, 0, 0, 0, 6, 6],
-		[0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0]]
+# entrances = [0, 1]
+# exits = [4, 5]
+# path = [[0, 0, 4, 6, 0, 0],
+#       [0, 0, 5, 2, 0, 0],
+#       [0, 0, 0, 0, 4, 4],
+#       [0, 0, 0, 0, 6, 6],
+#       [0, 0, 0, 0, 0, 0],
+#       [0, 0, 0, 0, 0, 0]]
 a = answer(entrances, exits, path)
 print(a)
-
-
