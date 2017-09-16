fd6cc0b7e3ecf63832346505addf795f7d74663c
David Ferris <david.j.ferris.21@gmail.com>
2017-09-16 15:57:08 -0400

    multiple channel plotting helper function

--- report_prototypes/rms_pca_comparison.py
+++ report_prototypes/rms_pca_comparison.py
@@ -13,6 +13,19 @@ def plot(Y, title=None, filename=None):
 	if filename != None:
 		plt.savefig('/Users/davidferris/Documents/Personal/Software/miscellaneous/{}'.format(filename))
 
+def plot_multiple(X, title=None, filename=None):
+	f, (ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8) = plt.subplots(8, sharex=True, sharey=True, figsize=(15,8))
+	
+	axes = (ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8)
+	for i,a in enumerate(axes):
+		a.plot(X[i])
+		a.get_yaxis().set_visible(False)
+
+	if title != None:
+		ax1.set_title(title)
+	if filename != None:
+		plt.savefig('/Users/davidferris/Documents/Personal/Software/miscellaneous/report_prototypes/{}'.format(filename))
+
 #Prep channel of X
 n = 500
 x = np.arange(n)
@@ -22,6 +35,8 @@ X[1] = np.sin(2 * np.pi * 3 * x / n)*2.2 +2.5
 for i in range(2,8):
 	X[i] = get_noise(n)
 X = np.asarray(X)
+
+plot_multiple(X, title='Simulated EMG Channels', filename='channels')
 
 #Perform RMS
 Y_rms = np.asarray([sqrt(abs(sum(t))) for t in X.T])
