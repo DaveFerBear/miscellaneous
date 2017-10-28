b708ee2a21e1554ffa851a3b02f3a7f1cdc87386
David Ferris <david.j.ferris.21@gmail.com>
2017-10-28 15:39:11 -0400

    updated readme, added endpoints

--- deeplearni.ng_challenge/README.md
+++ deeplearni.ng_challenge/README.md
@@ -1,3 +1,8 @@
 # Palindrome Web Server
 
-Some other content.
+### How to Build
+1. Install the necessary python packages: `pip install requirements.txt`
+2. Run the server: `python main.py`
+
+### Endpoint Documentation
+This is some endpoints...
--- deeplearni.ng_challenge/main.py
+++ deeplearni.ng_challenge/main.py
@@ -4,12 +4,16 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
-class Palindromes(Resource):
+class Palindrome(Resource):
 	def get(self):
 		return {'test': 'result'}
-class Palindrome_Count
-api.add_resource(Palindromes, '/palindromes')
-api.add_resource(Palindromes, '')
+
+class Palindrome_Count(Resource):
+	def get(self):
+		return 5
+
+api.add_resource(Palindrome, '/palindromes')
+api.add_resource(Palindrome_Count, '/palindromes/count')
 
 if __name__ == '__main__':
      app.run(port='5000')
