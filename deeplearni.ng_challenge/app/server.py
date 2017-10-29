cfbb8acf21491286236bb32a7d7db88ec948b25b
David Ferris <david.j.ferris.21@gmail.com>
2017-10-29 14:23:40 -0400

    clean up code

--- deeplearni.ng_challenge/app/server.py
+++ deeplearni.ng_challenge/app/server.py
@@ -8,9 +8,8 @@ api = Api(app)
 
 data_file = 'data/data.txt'
 
-def start(port, data_file_name):
-	data_file = data_file_name
 
+def start(port, data_file_name):
 	api.add_resource(PalindromeResource, '/palindromes')
 	api.add_resource(PalindromeCountResource, '/palindromes/count')
 	app.run(port=port)
@@ -22,6 +21,3 @@ class PalindromeResource(Resource):
 class PalindromeCountResource(Resource):
 	def get(self):
 		return jsonify(number_palindromes=Palindrome.get_num_palindromes(data_file))
-
-
-
